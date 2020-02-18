a34a843fa3330e35e0414e0ff96084a680f0b860
Edgar <edgarperezblanco@gmail.com>
2020-02-18 10:51:18 +0100

    end s1

--- Lab1/P25054.hs
+++ Lab1/P25054.hs
@@ -44,13 +44,56 @@ remove xs [] = xs
 remove [] ys = []
 remove (x:xs) ys
     | (elem x ys)   = (remove xs ys)
-    | otherwise     = (++) [x] (remove xs ys)
+    | otherwise     = [x] ++ (remove xs ys)
 
 
 -- 6. Juntar un conjunto de listas en una sola lista (concat)
 
- flatten :: [[Int]] -> [Int]
+flatten :: [[Int]] -> [Int]
 
- flatten (x:xs)
-    | xs == []  = x
-    | otherwise = (++) x (flatten xs)
+flatten [] = []
+flatten xs
+    |(tail xs) == []  = (head xs)
+    | otherwise         = (++) (head xs) (flatten (tail xs))
+
+flattenPro :: [[Int]] -> [Int] -- Version utilizando la funcion incluida
+flattenPro x = concat x
+
+
+-- 7. Separa una lista en dos listas diferentes, una con los numeros pares y otra con los impares
+
+oddsNevens :: [Int] -> ([Int],[Int])
+
+oddsNevens list = oddsNevens' list [] []
+    where
+        oddsNevens' :: [Int] -> [Int] -> [Int] -> ([Int],[Int])
+        oddsNevens' [] odd even = (odd, even)
+        oddsNevens' (elem:others) odd even     
+            | (mod elem 2) == 0  = oddsNevens' others odd (even ++ [elem])
+            | otherwise          = oddsNevens' others (odd ++ [elem]) even
+
+-- 8. Divisores primos de un natural > 0
+
+primeDivisors :: Int -> [Int]
+
+primeDivisors n = primeDivisors' [] 2
+    where
+        primeDivisors' :: [Int] -> Int -> [Int]
+        primeDivisors' divisors d
+            | d > n                             = divisors
+            | (d == 2) && ((mod n 2) == 0)      = primeDivisors' (divisors ++ [2]) 3
+            | (d == 2)                          = primeDivisors' divisors 3
+            | ((mod n d) == 0) && (isPrime d)   = primeDivisors' (divisors ++ [d]) (d+2)
+            | otherwise                         = primeDivisors' divisors (d+2)
+            where
+                isPrime :: Int -> Bool
+                isPrime 0 = False
+                isPrime 1 = False
+                isPrime 2 = True
+                isPrime n = isPrimeAux (floor (sqrt (fromIntegral n)))
+                    where
+                        isPrimeAux :: Int -> Bool
+                        isPrimeAux d
+                            | d == 1        = True
+                            | mod n d == 0  = False
+                            | otherwise     = isPrimeAux (d-1)
