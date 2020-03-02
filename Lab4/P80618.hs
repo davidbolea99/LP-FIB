86d647dde19cd3a60ac3165f188e3c3545ab33d0
Edgar <edgarperezblanco@gmail.com>
2020-03-02 19:20:36 +0100

    Lab4 done

--- Lab4/P80618.hs
+++ Lab4/P80618.hs
@@ -1,6 +1,9 @@
 data Queue a = Queue [a] [a]
      deriving (Show)
 
+
+-- PART 1
+
 create :: Queue a
 create = Queue [] []
 
@@ -8,14 +11,24 @@ push :: a -> Queue a -> Queue a
 push x (Queue s t) = (Queue s (x:t))
 
 pop :: Queue a -> Queue a
-pop (Queue s t) = (Queue xs [])
-    where
-        (x:xs) = s ++ reverse(t)
+pop (Queue [] t) = (Queue (drop 1 (reverse t)) [])
+pop (Queue s t) = (Queue (drop 1 s) t)
 
 top :: Queue a -> a
-top (Queue [] t) = head $ reverse t
+top (Queue [] t) = last t
 top (Queue (x:xs) t) = x
 
 empty :: Queue a -> Bool
 empty (Queue [] []) = True
 empty (Queue s t) = False
+
+
+-- PART 2
+
+instance Eq a => Eq (Queue a)
+    where
+        (Queue [] []) == (Queue [] []) = True
+        q1 == q2
+            | (empty q1) || (empty q2)  = False
+            | (top q1) == (top q2)      = (pop q1) == (pop q2)
+            | otherwise                 = False
