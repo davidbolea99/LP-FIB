03fe51c050f70ff6a4be12282cd6ad0f147c8a7e
foster99 <edgarperezblanco@gmail.com>
2020-04-11 22:49:52 +0200

    juego funcional

--- Haskell/Practica/joc.hs
+++ Haskell/Practica/joc.hs
@@ -23,11 +23,11 @@ instance Show Board where
             header  = "\n     " ++ (intercalate "   " $ string2list $ take width ['A'..]) ++ "\n"
             separated_board = intercalate border_in $ map colSeparator (board2string board)
 
-            border_up   = " \n   " ++ rowSeparator ++ "| "
-            border_in   = " |\n   " ++ rowSeparator ++ "| "
-            border_down = " |\n   " ++ rowSeparator
+            border_up   =  " " ++ rowSeparator ++ "| "
+            border_in   = " |" ++ rowSeparator ++ "| "
+            border_down = " |" ++ rowSeparator
 
-            rowSeparator = "+" ++ (intercalate "+" $ take width $ repeat "---") ++ "+\n   "
+            rowSeparator = "\n   +" ++ (intercalate "+" $ take width $ repeat "---") ++ "+\n   "
             colSeparator = intercalate " | " . map (\x -> [x])
 
 type Cell = Maybe Player
@@ -40,7 +40,7 @@ showCell (Just O) = 'O'
 
 data AI = RANDOM | GREEDY | SMART
 
-
+---------------------------------------------------------------------------------------------------------------
 
 main :: IO ()
 main = do
@@ -56,6 +56,7 @@ main = do
 
     end_game result
 
+---------------------------------------------------------------------------------------------------------------
 
 -- Bucle principal del juego. Este recibe como entrada el tablero actual, el nivel de
 -- la cpu, y un booleano que indica si le toca al usuario (True) o a la cpu (False)
@@ -68,10 +69,17 @@ loop board ai ((X,move):moves) = do
     user_move <- move ai board
     let newBoard = put_piece X (string2int user_move) board
 
-    let end = check_win newBoard
+    let end = check_win X newBoard
     
-    if end then do return True
-    else        do loop newBoard ai moves
+    if end then do
+        putStrLn $ show newBoard
+        return True
+    else do 
+        loop newBoard ai moves
+
+---------------------------------------------------------------------------------------------------------------
+
+-- Para jugar en modo2 jugadores (debugging)
 loop board ai ((O,move):moves) = do
     
     putStrLn $ show board
@@ -80,16 +88,56 @@ loop board ai ((O,move):moves) = do
     user_move <- move ai board
     let newBoard = put_piece O (string2int user_move) board
 
-    let end = check_win newBoard
+    let end = check_win O newBoard
     
-    if end then do return True
-    else        do loop newBoard ai moves
+    if end then do
+        putStrLn $ show newBoard
+        return True
+    else do 
+        loop newBoard ai moves
+
+---------------------------------------------------------------------------------------------------------------
 
 string2int :: String -> Int
 string2int c = (ord (head c)) - (ord 'A')
 
-check_win :: Board -> Bool
-check_win _ = False
+---------------------------------------------------------------------------------------------------------------
+
+data Direction = H | V | BU | UB
+    deriving Eq
+
+check_win :: Player -> Board -> Bool
+check_win player board = check_hor || check_vert || check_diag_BU || check_diag_UB
+    where
+        check_hor       = (max_length player H  board) >= 4
+        check_vert      = (max_length player V  board) >= 4
+        check_diag_BU   = (max_length player BU board) >= 4
+        check_diag_UB   = (max_length player UB board) >= 4
+
+max_length :: Player -> Direction -> Board -> Int
+max_length p dir board@(Board heigth width tops _)
+    | dir == H  = maximum [ max_length_bools [ get (i,j) board == Just p | j <- [0..(heigth-1)] ] | i <- [0..(width-1) ] ]
+    | dir == V  = maximum [ max_length_bools [ get (i,j) board == Just p | i <- [0..(width-1) ] ] | j <- [0..(heigth-1)] ]
+    | dir == BU = maximum [ max_length_bools [ get (i,j) board == Just p | i <- [0..(width-1) ], j <- [0..(heigth-1)], i+j == n ] | n <-[3..(width+heigth-5)] ]
+    | dir == UB = maximum [ max_length_bools [ get (i,j) board == Just p | i <- [0..(width-1) ], j <- [0..(heigth-1)], i-j == n ] | n <-[-(width-4)..(heigth-4)] ]
+        where
+            max_length_bools :: [Bool] -> Int
+            max_length_bools list = max_length_bools' 0 0 list
+                where
+                    max_length_bools' :: Int -> Int -> [Bool] -> Int
+                    max_length_bools' _ max [] = max
+                    max_length_bools' accum max (x:xs)
+                        | new_accum > max     = max_length_bools' new_accum new_accum xs 
+                        | otherwise               = max_length_bools' new_accum max xs
+                        where
+                            new_accum
+                                | x         = accum + 1
+                                | otherwise = 0
+        
+
+max_length _ _ _ = 0
+
+---------------------------------------------------------------------------------------------------------------
 
 put_piece :: Player -> Int -> Board -> Board
 put_piece p col board@(Board h w tops cells)
@@ -101,6 +149,7 @@ put_piece p col board@(Board h w tops cells)
             new_cells       = Map.insert (row,col) p cells
             new_tops        = replaceNth col (row - 1) tops
 
+---------------------------------------------------------------------------------------------------------------
 
 -- Definimos move_list como una lista infinita de movimientos alternados entre el jugador y la IA.
 -- Cada movimiento es una tupla formada por el jugador que mueve y una funcion que retorna el
@@ -111,8 +160,12 @@ move_list :: [(Player, AI -> Board -> IO String)]
 --move_list =  cycle [ (X, \_ _ -> getLine), (O, \ai board -> return $ ai_move ai board)]
 move_list =  cycle [ (X, \_ _ -> getLine), (O, \_ _ -> getLine)]
 
+---------------------------------------------------------------------------------------------------------------
+
 ai_move :: AI -> Board -> Int
 ai_move ai board = 0
+
+---------------------------------------------------------------------------------------------------------------
 
 -- Funcion que trata la situacion de final del juego, donde el usuario puede decidir
 -- si quiere jugar de nuevo o no.
@@ -133,6 +186,8 @@ end_game result = do
     else do
         end_game result
 
+---------------------------------------------------------------------------------------------------------------
+
 -- Lectura del tamano del tablero
 read_size:: IO (Int,Int)
 read_size = do
@@ -150,7 +205,9 @@ read_size = do
         read_size
     else
         return (n,m)
-        
+
+---------------------------------------------------------------------------------------------------------------
+
 -- Seleccion de la dificultad del oponente
 read_ai:: IO AI
 read_ai = do
@@ -176,6 +233,8 @@ read_ai = do
         putStrLn "Entrada no valida. Introduzca '1', '2' o '3'."
         read_ai
 
+---------------------------------------------------------------------------------------------------------------
+
 -- Funcion que retorna un Board de tamano N x N vacio (lleno de espacios)
 empty_board :: Int -> Int -> Board
 empty_board n m = Board n m tops Map.empty
@@ -186,21 +245,27 @@ empty_board n m = Board n m tops Map.empty
 string2list :: String -> [String]
 string2list = map (\x -> [x])
 
+---------------------------------------------------------------------------------------------------------------
+
 -- Funcion que convierte el Map que representa el tablero en una lista de strings con los chars
 -- asociados a cada celda. Los elementos estan ordenados del mismo modo que acostrumbramos a leer
 -- una matriz en NxM: Primero el elemento [0,0], [0,1], [0,2], etc.
+
 board2string :: Board -> [String]
 board2string board@(Board heigth width _ _) = [row_elements row | row <- [0 .. heigth - 1]]
         where
             row_elements row = [cell row col | col <- [0 .. width - 1]]
-            cell row col = showCell ((get row col) board)
+            cell row col = showCell ((get (row,col)) board)
+
+---------------------------------------------------------------------------------------------------------------
 
 -- Funcion que encapsula aquella funcion que dada un tablero, retorna el elemento [i,j]. Utiliza
 -- la funcion lookup de la clase Map. Aplica la busqueda sobre el Map asociaciado al Board (3r 
 -- elemento de su struct)
-get :: Int -> Int -> (Board -> Maybe Player)
-get i j = Map.lookup (i,j) . cells
+get :: (Int,Int) -> (Board -> Maybe Player)
+get coord = Map.lookup coord . cells
 
+---------------------------------------------------------------------------------------------------------------
 
 replaceNth :: Int -> a -> [a] -> [a]
 replaceNth _ _ [] = []
